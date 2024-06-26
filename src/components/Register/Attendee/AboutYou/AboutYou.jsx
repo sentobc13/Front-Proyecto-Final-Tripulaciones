a3622f56eee345c664726e13340bf71158b66eb1
sentobc13 <sentoberlanga@gmail.com>
2024-06-26 15:28:10 +0200

    about you cambios

--- src/components/Register/Attendee/AboutYou/AboutYou.jsx
+++ src/components/Register/Attendee/AboutYou/AboutYou.jsx
@@ -1,6 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {
+    Box,
+    Button,
+    FormControl,
+    FormLabel,
+    Input,
+    Stack,
+    Heading,
+    Flex,
+    useToast,
+  } from '@chakra-ui/react';
 
 const AboutYou = () => {
+    const [formData, setFormData] = useState({
+        nombre: '',
+        apellido: '',
+        país_de_residencia: '',
+        empresa: '',
+        telefono: '',
+        cargo: '',
+        linkedin: '',
+
+    })
   return (
     <div>AboutYou</div>
   )
