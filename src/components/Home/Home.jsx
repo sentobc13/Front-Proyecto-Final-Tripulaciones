853b61141baf2929901a286b083db2af435d5dd6
Patricia 607413324 <patriciapatriciaariasburdeos@gmail.com>
2024-06-26 12:12:27 +0200

    Aplicando estilos y contenido a componente

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,18 @@
+import './Home.scss'; 
+
+import logo1 from '../../assets/E-learning-Experience_Logo-Blanco.png';
 
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="home-container">
+      <img src={logo1} alt="Logo" className="logo" />
+      <div className="buttons">
+        <button className="button">Soy visitante</button>
+        <button className="button">Soy ponente</button>
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
