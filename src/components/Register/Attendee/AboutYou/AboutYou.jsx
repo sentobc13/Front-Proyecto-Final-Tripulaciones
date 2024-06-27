7d291ed58fa5719c486deab8940467a20a6af952
sentobc13 <sentoberlanga@gmail.com>
2024-06-27 10:24:14 +0200

    abouy you se ve

--- src/components/Register/Attendee/AboutYou/AboutYou.jsx
+++ src/components/Register/Attendee/AboutYou/AboutYou.jsx
@@ -1,32 +1,42 @@
-import React, { useState } from 'react'
-import {
-    Box,
-    Button,
-    FormControl,
-    FormLabel,
-    Input,
-    Stack,
-    Heading,
-    Flex,
-    useToast,
-  } from '@chakra-ui/react';
+import React from 'react'
+import { Link } from 'react-router-dom'
 
-  <div>
-    <h3>Sobre ti</h3>
-  </div>
 const AboutYou = () => {
-    const [formData, setFormData] = useState({
-        nombre: '',
-        apellido: '',
-        país_de_residencia: '',
-        empresa: '',
-        telefono: '',
-        cargo: '',
-        linkedin: '',
-
-    })
   return (
-    <div>AboutYou</div>
+    <>
+     <div>
+        <h2>Sobre ti</h2>
+        </div>   
+
+        <div className='containerAboutYouForm'>
+            <p>
+                <input type="text" name='nombre' value='nombre' /> Nombre
+            </p>
+            <p>
+                <input type="text" name='nombre' value='nombre' /> Apellidp
+            </p>
+            <p>
+                <input type="text" name='nombre' value='nombre' /> País de residencia
+            </p>
+            <p>
+                <input type="text" name='nombre' value='nombre' /> Empresa
+            </p>
+            <p>
+                <input type="text" name='nombre' value='nombre' /> Teléfono
+            </p>
+            <p>
+                <input type="text" name='nombre' value='nombre' /> Cargo
+            </p>
+            <p>
+                <input type="text" name='nombre' value='nombre' /> Linkedin
+            </p>
+        </div>
+        <div className='BadgecontainerButton'>
+                <Link to="/allergies">
+                    <button>Siguiente</button>
+                </Link>
+            </div>
+    </>
   )
 }
 
