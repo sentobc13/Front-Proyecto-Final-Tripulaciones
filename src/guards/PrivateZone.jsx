e689b16c4dd9b07a316ef40f20bcc5d9cd278048
Sebastian <sebas33696@gmail.com>
2024-06-27 16:49:51 +0200

    modificado guard

--- src/guards/PrivateZone.jsx
+++ src/guards/PrivateZone.jsx
@@ -2,7 +2,7 @@ import { Navigate } from "react-router-dom"
 
 const PrivateZone = ({ children }) => {
   const logged = localStorage.getItem("Attendee") || localStorage.getItem("Speaker") 
-  return logged ? children : <Navigate to="/identify" />;
+  return logged ? children : <Navigate to="/loginprincipal" />;
 };
 
 export default PrivateZone
