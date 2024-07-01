bf6aaf5478ec2763521fef5a8a27b2316ce9b14b
Patricia 607413324 <patriciapatriciaariasburdeos@gmail.com>
2024-07-01 19:15:04 +0200

    Aplicando mas cambios

--- src/app/store.js
+++ src/app/store.js
@@ -2,15 +2,15 @@ import { configureStore } from '@reduxjs/toolkit';
 import authAttendee from '../features/auth/attendee/authAttendeeSlice';
 import authSpeaker from '../features/auth/speaker/authSpeakerSlice';
 import WorkshopSlice from '../features/workshop/WorkshopSlice';
+import ticketsReducer from '../features/tickets/ticketsSlice';
 
 const store = configureStore({
     reducer: {
         authAttendee,
         authSpeaker,
-        WorkshopSlice
+        WorkshopSlice,
+        tickets: ticketsReducer, 
     },
-  });
-  
-  export default store;
+});
 
-  
+export default store;
--- src/components/RegisterTercerPaso/RegisterTercerPaso.jsx
+++ src/components/RegisterTercerPaso/RegisterTercerPaso.jsx
@@ -1,15 +1,41 @@
+import { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import './RegisterTercerPaso.scss';
 import { Link } from 'react-router-dom';
 import { MdArrowBackIos } from 'react-icons/md';
-import { Accordion, AccordionItem, AccordionButton, AccordionPanel, Box, Button, AccordionIcon } from '@chakra-ui/react';
+import { Accordion, AccordionItem, AccordionButton, AccordionPanel, Box, AccordionIcon } from '@chakra-ui/react';
+import { getAllTickets } from '../../features/tickets/ticketsSlice';
+import { Spinner } from '@chakra-ui/react';
 
 const RegisterTercerPaso = () => {
+    const [selectedPrice, setSelectedPrice] = useState('');
+    const dispatch = useDispatch();
+    const { isLoading, isError, errorMessage } = useSelector((state) => state.tickets);
+
+    useEffect(() => {
+        const savedPrice = localStorage.getItem('selectedPrice');
+        if (savedPrice) {
+            setSelectedPrice(savedPrice);
+        }
+    }, []);
+
+    useEffect(() => {
+        if (selectedPrice) {
+            localStorage.setItem('selectedPrice', selectedPrice);
+        }
+    }, [selectedPrice]);
+
+    const handlePriceClick = (price) => {
+        setSelectedPrice(price);
+        dispatch(getAllTickets({ price }));
+    };
+
     return (
         <>
             <div className="register-tercer-paso-container">
                 <div className="stepper">
                     <div className="btn-back">
-                        <Link to="/registerSegundoPaso" style={{ textDecoration: 'none', color: 'inherit', }}>
+                        <Link to="/registerSegundoPaso" style={{ textDecoration: 'none', color: 'inherit' }}>
                             <MdArrowBackIos style={{ fontSize: 18 }} />
                         </Link>
                     </div>
@@ -22,34 +48,58 @@ const RegisterTercerPaso = () => {
             </div>
 
             <div className="containerRegister3">
-                <div className='div-title'>
-                    <p className='title'>Selecciona el tipo de entrada</p>
+                <div className="div-title">
+                    <p className="title">Selecciona el tipo de entrada</p>
                 </div>
             </div>
             <div className="container-content3">
-                <div className='div-subtitle'>
-                    <p className='subtitle'>Las entradas incluyen:</p>
+                <div className="div-subtitle">
+                    <p className="subtitle">Las entradas incluyen:</p>
                     <ul>
                         <li>Welcome Pack del Asistente</li>
                         <li>Asistencia a ponencias y clases magistrales.</li>
                         <li>Coffee Break, Comida y Cena Cóctel y copas (Cada día)</li>
                     </ul>
                 </div>
-                <Accordion allowToggle className='div-entry-type'>
+                <Accordion allowToggle className="div-entry-type">
                     <AccordionItem>
                         <h2>
                             <AccordionButton>
-                                <Box as='span' flex='1' textAlign='left'>
+                                <Box as="span" flex="1" textAlign="left">
                                     Administración pública, educación y entidades sin ánimo de lucro
                                 </Box>
                                 <AccordionIcon />
                             </AccordionButton>
                         </h2>
                         <AccordionPanel pb={4}>
-                            <div className='div-prices'>
-                                <p>Día 1: 23 de junio <Button colorScheme="blue" variant="solid">180€</Button></p>
-                                <p>Día 2: 24 de junio <Button colorScheme="blue" variant="solid">180€</Button></p>
-                                <p>2 días: 23 y 24 de junio <Button colorScheme="blue" variant="solid">380€</Button></p>
+                            <div className="div-prices">
+                                <p>
+                                    Día 1: 23 de junio{' '}
+                                    <button
+                                        className={`price-button ${selectedPrice === '180-1' ? 'selected' : ''}`}
+                                        onClick={() => handlePriceClick('180-1')}
+                                    >
+                                        180€
+                                    </button>
+                                </p>
+                                <p>
+                                    Día 2: 24 de junio{' '}
+                                    <button
+                                        className={`price-button ${selectedPrice === '180-2' ? 'selected' : ''}`}
+                                        onClick={() => handlePriceClick('180-2')}
+                                    >
+                                        180€
+                                    </button>
+                                </p>
+                                <p>
+                                    2 días: 23 y 24 de junio{' '}
+                                    <button
+                                        className={`price-button ${selectedPrice === '380-1' ? 'selected' : ''}`}
+                                        onClick={() => handlePriceClick('380-1')}
+                                    >
+                                        380€
+                                    </button>
+                                </p>
                             </div>
                         </AccordionPanel>
                     </AccordionItem>
@@ -57,29 +107,57 @@ const RegisterTercerPaso = () => {
                     <AccordionItem>
                         <h2>
                             <AccordionButton>
-                                <Box as='span' flex='1' textAlign='left'>
+                                <Box as="span" flex="1" textAlign="left">
                                     Empresa privada
                                 </Box>
                                 <AccordionIcon />
                             </AccordionButton>
                         </h2>
                         <AccordionPanel pb={4}>
-                            <div className='div-prices'>
-                                <p>Día 1: 23 de junio <Button colorScheme="blue" variant="solid">180€</Button></p>
-                                <p>Día 2: 24 de junio <Button colorScheme="blue" variant="solid">180€</Button></p>
-                                <p>2 días: 23 y 24 de junio <Button colorScheme="blue" variant="solid">380€</Button></p>
+                            <div className="div-prices">
+                                <p>
+                                    Día 1: 23 de junio{' '}
+                                    <button
+                                        className={`price-button ${selectedPrice === '180-3' ? 'selected' : ''}`}
+                                        onClick={() => handlePriceClick('180-3')}
+                                    >
+                                        180€
+                                    </button>
+                                </p>
+                                <p>
+                                    Día 2: 24 de junio{' '}
+                                    <button
+                                        className={`price-button ${selectedPrice === '180-4' ? 'selected' : ''}`}
+                                        onClick={() => handlePriceClick('180-4')}
+                                    >
+                                        180€
+                                    </button>
+                                </p>
+                                <p>
+                                    2 días: 23 y 24 de junio{' '}
+                                    <button
+                                        className={`price-button ${selectedPrice === '380-2' ? 'selected' : ''}`}
+                                        onClick={() => handlePriceClick('380-2')}
+                                    >
+                                        380€
+                                    </button>
+                                </p>
                             </div>
                         </AccordionPanel>
                     </AccordionItem>
                 </Accordion>
+                {isLoading && <Spinner size="xl" color="blue.500" thickness="4px" />}
+                {isError && (
+                    <p>Hubo un error al guardar la opción seleccionada: {errorMessage}</p>
+                )}
             </div>
-            <div className='container-button'>
+            <div className="container-button">
                 <Link to="/connectLinkedin">
-                    <button className='continue-button'>Siguiente</button>
+                    <button className="continue-button">Siguiente</button>
                 </Link>
             </div>
         </>
     );
-}
+};
 
 export default RegisterTercerPaso;
