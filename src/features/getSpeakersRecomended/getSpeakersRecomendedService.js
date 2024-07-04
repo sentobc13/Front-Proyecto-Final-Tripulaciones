75424ed8ea975a0f521af21d0329bccf29af93a1
Sebastian <sebas33696@gmail.com>
2024-07-04 13:13:02 +0200

    agregado url de data

--- src/features/getSpeakersRecomended/getSpeakersRecomendedService.js
+++ src/features/getSpeakersRecomended/getSpeakersRecomendedService.js
@@ -2,20 +2,19 @@ import axios from "axios"
 
 const user = JSON.parse(localStorage.getItem('attendee')) || JSON.parse(localStorage.getItem('speaker'));
 
-const getSpeakersRecomended = async ()=>{
-    const token = localStorage.getItem("token");
-    const res = await axios.get("https://api-match-1.onrender.com/similarity/" + user._id ,{
-      headers: {
-        authorization: token,
-      },
-    })
-    return res.data
+const getSpeakersRecomended = async () => {
+  const id = {
+    "id": user.id
   }
 
+  const res = await axios.post("https://api-match-1.onrender.com/similarity/", id)
+  return res.data
+}
 
-  const getSpeakersRecomendedService = {
-    getSpeakersRecomended
-  }
-  
-  
-  export default getSpeakersRecomendedService
+
+const getSpeakersRecomendedService = {
+  getSpeakersRecomended
+}
+
+
+export default getSpeakersRecomendedService
